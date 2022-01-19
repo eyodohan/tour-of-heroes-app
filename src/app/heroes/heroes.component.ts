9e22126a841f0c6c9c5a56af5069144541fb0781
eyodohan <eyodohan@gmail.com>
2022-01-19 19:53:32 +0300

    component ve service ekleme

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from "../hero"
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 
 @Component({
@@ -10,17 +11,26 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private heroService :HeroService,
+    private messageService: MessageService
+    ) { }
 
   selectedHero?: Hero;
   onSelect(hero:Hero):void{
     this.selectedHero = hero
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
     console.log(hero);
   }
 
-  heroes=HEROES;
+  heroes:Hero[]=[];
+
+  getHeroes():void {
+    this.heroService.getHeroes().subscribe(heroes=>this.heroes=heroes)
+  }
 
   ngOnInit(): void {
+    this.getHeroes()
   }
 
 }
